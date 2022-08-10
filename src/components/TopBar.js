1d456045613c68a2e9a6d5ec6ab95f9fbabd947a
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-09 22:45:07 -0300

    expandir com os resultados

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,46 @@
 import styled from 'styled-components';
+import {DebounceInput} from 'react-debounce-input'
+import React from 'react';
+import axios from 'axios';
+import {BsSearch} from 'react-icons/bs'
 
 export default function TopBar(){
+    const [expended,setExpended] = React.useState(false)
+    const [data, setData] =React.useState()
+    function search(value){
+        const body = {
+            value
+        }
+        if(value !== ''){
+            const promise = axios.get('https://pokeapi.co/api/v2/pokemon/?limit=40',body)
+            promise.then((req)=> {
+                setData(req.data)
+                setExpended(true)
+                console.log(req.data)
+            })
+
+        }
+       setExpended(false)
+        
+    }
     return(
         <Top>
             <h1>linkr</h1>
-                <input
-                 placeholder='Search for people'
-                 />
+            <Container teste={expended? '45px' : '45px'}>
+                <Search>
+                    <DebounceInput
+                        minLength={3}
+                        debounceTimeout={300}
+                        onChange={e => search(e.target.value)}
+                        placeholder='Search for people'
+                    ></DebounceInput>
+                    <BsSearch color='#C6C6C6' size='20px'></BsSearch>
+                </Search>
+                {expended?<Itens>{data.results.map((e)=><p>{e.name}</p>)}</Itens> : <></>}
+                
+                
+            </Container>
+                
             <img src='https://sm.ign.com/ign_br/screenshot/default/naruto-shippuden_f134.png'></img>
         </Top>
     )
@@ -16,9 +50,9 @@ const Top = styled.div`
 height: 70px;
 background-color: #151515;
 display: flex;
-align-items: center;
-padding:0px 30px 0px;
+padding:10px 30px 0px;
 justify-content: space-between;
+z-index: 1;
 
 h1{
     font-family: 'Passion One';
@@ -32,19 +66,45 @@ img{
     height: 53px;
     object-fit: cover;
 }
-input{
-    width: 560px;
-    min-width: 375px;
-    max-width:560px;
-    background: #FFFFFF;
-    border-radius: 8px;
-    height: 45px;
-    border:none;
+
+
+` 
+const Container = styled.div`
+    width: 500px;
     display: flex;
-    flex-wrap: wrap;
+    background-color: #fff;
+    border-radius: 8px;
+    height: ${props => props.teste };
+    z-index: 2;
+    flex-direction: column;
+    
     @media (max-width: 610px) {
         display: none;
     }
+   
+`
+const Search = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    input{
+        width: 400px;
+        min-width: 365px;
+        background: #FFFFFF;
+        border-radius: 8px 0px 0px 8px;
+        height: 40px;
+        border:none;
+        display: flex;
+        flex-wrap: wrap;
+    
     }
+`
+const Itens = styled.div`
+   background-color: #E7E7E7;
+   z-index: 1;
+   padding: 10px;
+    min-height: 200px;
+   overflow: scroll;
+   border-radius: 0px 0px 8px 8px;
 
-` 
+`
