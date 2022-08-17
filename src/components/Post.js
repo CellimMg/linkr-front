17e5aa5bfd98e95d7c8f69edaa3c916332439a24
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-08-17 19:58:20 -0300

    [fix]ajuste responsividade

--- src/components/Post.js
+++ src/components/Post.js
@@ -151,42 +151,42 @@ export default function Post(props){
     return(
         <PrincipalContainer>
             
-        <ContainerPost comments={expendedComments? '0px': '0px'}>
-            <Left>
-                <img src={dataPost.userImage} alt='profile'></img>
-                <Icon onClick={(e)=> {load?doNothin():like(dataPost.postId)}} > 
-                    {liked? <AiFillHeart color='red'/>:<AiOutlineHeart/>} 
-                    <h6 data-tip={usersLikes} > {countLikes} {countLikes <= 1? <>like</>:<>likes</>}</h6>   
-                    <ReactTooltip place='bottom' effect='solid' className='toolTip' arrowColor=' rgba(255, 255, 255, 0.9);d'/>
-                </Icon>
-                <Comments numberCommnets={dataPost.whoComments === null? 0 : dataPost.whoComments.length} setExpendedComments={setExpendedComments} expendedComments={expendedComments}/>
-            </Left>
-            
-            <Content editable={editable}>
-                <div className='topo'>
-                    <Link to={`/user/${dataPost.userId}`}><h1>{dataPost.userName}</h1></Link>
-                    <div>
-                        {dataPost.userId === user.data.id ? <><AiOutlineEdit color='white' size='24px' onClick={() => [setEditable(!editable), inputRef.current.focus()]}/> <IoTrashOutline color='white' size='24px' onClick={deleteModal}/></> : <span></span>}
-                    </div>
-                </div>
-                <div className='postDescription'>
-                    <textarea readOnly={editable}  onBlur={() => [console.log(dataPost.description), setDescription(props.postData.description)]} onChange={(event) => setDescription(event.target.value)} value={description} onKeyDown={handleKeyDown} ref={inputRef}/>
-                </div>
-                <a href={dataPost.link} target="_blank">
-                    <div className='linkBody'>
-                        <div className='linkText'>
-                            <h2>{dataPost.urlTitle}</h2>
-                            <h5>{dataPost.urlDescription}</h5>
-                            <h4>{dataPost.link}</h4>
-                        </div>
-                        <div className='linkImage'>
-                            <img src={dataPost.urlImage}/>
+            <ContainerPost comments={expendedComments? '0px': '0px'}>
+                <Left>
+                    <img src={dataPost.userImage} alt='profile'></img>
+                    <Icon onClick={(e)=> {load?doNothin():like(dataPost.postId)}} > 
+                        {liked? <AiFillHeart color='red'/>:<AiOutlineHeart/>} 
+                        <h6 data-tip={usersLikes} > {countLikes} {countLikes <= 1? <>like</>:<>likes</>}</h6>   
+                        <ReactTooltip place='bottom' effect='solid' className='toolTip' arrowColor=' rgba(255, 255, 255, 0.9);d'/>
+                    </Icon>
+                    <Comments numberCommnets={dataPost.whoComments === null? 0 : dataPost.whoComments.length} setExpendedComments={setExpendedComments} expendedComments={expendedComments}/>
+                </Left>
+                
+                <Content editable={editable}>
+                    <div className='topo'>
+                        <Link to={`/user/${dataPost.userId}`}><h1>{dataPost.userName}</h1></Link>
+                        <div>
+                            {dataPost.userId === user.data.id ? <><AiOutlineEdit color='white' size='24px' onClick={() => [setEditable(!editable), inputRef.current.focus()]}/> <IoTrashOutline color='white' size='24px' onClick={deleteModal}/></> : <span></span>}
                         </div>
                     </div>
-                </a>
-            </Content>
-        </ContainerPost> 
-        {expendedComments?<CommentsExpended postId={dataPost.postId} dataPost={dataPost}/> :<></>} 
+                    <div className='postDescription'>
+                        <textarea readOnly={editable}  onBlur={() => [console.log(dataPost.description), setDescription(props.postData.description)]} onChange={(event) => setDescription(event.target.value)} value={description} onKeyDown={handleKeyDown} ref={inputRef}/>
+                    </div>
+                    <a href={dataPost.link} target="_blank">
+                        <div className='linkBody'>
+                            <div className='linkText'>
+                                <h2>{dataPost.urlTitle}</h2>
+                                <h5>{dataPost.urlDescription}</h5>
+                                <h4>{dataPost.link}</h4>
+                            </div>
+                            <div className='linkImage'>
+                                <img src={dataPost.urlImage}/>
+                            </div>
+                        </div>
+                    </a>
+                </Content>
+            </ContainerPost> 
+            {expendedComments?<CommentsExpended postId={dataPost.postId} dataPost={dataPost}/> :<></>} 
         
         </PrincipalContainer>
     )
@@ -197,7 +197,9 @@ const PrincipalContainer= styled.div`
     position: relative;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    @media (max-width: 610px){
+        align-items: center;
+    }
     `
 
 const ContainerPost = styled.div`
